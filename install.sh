798e975d948cbe64dcc99dab1a4c098618b9adc1
knightrider5 <79694828+knightrider5@users.noreply.github.com>
2021-09-20 11:51:04 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#! bin/bash
+sudo su
+sudo apt-get install git -y 2> /dev/null
+sudo apt-get install python3 -y 2> /dev/null
+sudo apt-get install python3-pip -y 2> /dev/null
+sudo apt-get install golang-go -y 2> /dev/null
+sudo apt install cmake -y 2> /dev/null
+sudo apt install jq -y 2> /dev/null
+sudo apt-get install -y parallel
+sleep 2s
+clear
+echo "Basic Setup Done"
+
+go get github.com/tomnomnom/waybackurls && cp gf /usr/local/bin
+go get -u -v github.com/lc/gau && cp gau /usr/local/bin
+go get -u github.com/tomnomnom/gf && cp gf /usr/local/bin
+git clone https://github.com/1ndianl33t/Gf-Patterns && mkdir .gf
+cd Gf-Patterns/
+cp * /root/.gf
+cd ..
+sudo su
+rm Gf-Patterns/
